f43f0ae65e2cf5c465db49fcb3f3b7b046cdbf46
Trọng Hiếu <hieu15904@gmail.com>
2026-09-03 11:23:50 +0700

    tích hợp pug

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+// Cấu himhf thư mục mặc định chứa giao diện 
+app.set('views', path.join(__dirname, 'views'));
+// Cấu hình sử dụng Pug làm engine để render giao diện
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.send('Trang chủ');
+  res.render('client/pages/home.pug', {
+    pageTitle: 'Trang chủ',
+  });
 });
+
 app.get('/tour', (req, res) => {
-  res.send('chi tiết tour');
+  res.render('client/pages/tour.pug', {
+    pageTitle: 'Danh sách tour',
+  });
 });
 
 app.listen(port, () => {
