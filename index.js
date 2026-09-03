dd941300e5d1cc66ed03af239ae2c3fe53ab55d6
Trọng Hiếu <hieu15904@gmail.com>
2026-09-03 10:49:17 +0700

    thêm nodemon

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Trang chủ');
+});
+app.get('/tour', (req, res) => {
+  res.send('chi tiết tour');
 });
 
 app.listen(port, () => {
