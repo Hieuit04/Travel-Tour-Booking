521313c1c30e29685d39ec4ad2bcc585cad9e854
Trọng Hiếu <hieu15904@gmail.com>
2026-09-04 14:53:49 +0700

    thêm trang setting-account-admin-create, setting-account-admin-list, setting-list, setting-role-create, setting-role-list, setting-website-info

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -7,6 +7,7 @@ const tourRouters = require('./tour.route');
 const orderRouters = require('./order.route');
 const userRouters = require('./user.route');
 const contactRouters = require('./contact.route');
+const settingRouters = require('./setting.route');
 
 router.use('/account', accountRouters);
 router.use('/dashboard', dashboardRouters);
@@ -15,6 +16,7 @@ router.use('/tour', tourRouters);
 router.use('/order', orderRouters);
 router.use('/user', userRouters);
 router.use('/contact', contactRouters);
+router.use('/setting', settingRouters);
 
 module.exports = router;
 
