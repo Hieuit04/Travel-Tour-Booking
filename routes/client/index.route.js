df0f79f0d97fd3a8ad5c23c0ead6a46ca16e2460
Trọng Hiếu <hieu15904@gmail.com>
2026-09-04 09:56:01 +0700

    tích hợp trang giỏ hàng

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const tourRouters = require('./tour.route');
 const homeRouters = require('./home.route');
+const cartRouters = require('./cart.route');
 
-router.use('/', homeRouters);
 router.use('/tour', tourRouters);
+router.use('/cart', cartRouters);
+router.use('/', homeRouters);
 
 module.exports = router;
 
