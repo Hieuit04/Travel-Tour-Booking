ebe3baafd2b3fa1f4e1dd7c92210c048c5761b82
Trọng Hiếu <hieu15904@gmail.com>
2026-09-04 09:23:27 +0700

    tích hợp trang chi tiết tour

--- routes/client/tour.route.js
+++ routes/client/tour.route.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const tourController = require('../../controllers/client/tour.controller');
 
 router.get('/', tourController.list);
-
+router.get('/detail', tourController.detail);
 module.exports = router;
