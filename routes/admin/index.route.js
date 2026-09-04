d7164b4d0e0daf15daf1dc8e6b1a13d66c247a06
Trọng Hiếu <hieu15904@gmail.com>
2026-09-04 14:19:22 +0700

    thêm user list

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,12 +5,14 @@ const dashboardRouters = require('./dashboard.route');
 const categoryRouters = require('./category.route');
 const tourRouters = require('./tour.route');
 const orderRouters = require('./order.route');
+const userRouters = require('./user.route');
 
 router.use('/account', accountRouters);
 router.use('/dashboard', dashboardRouters);
 router.use('/category', categoryRouters);
 router.use('/tour', tourRouters);
 router.use('/order', orderRouters);
+router.use('/user', userRouters);
 
 module.exports = router;
 
