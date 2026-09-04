dfe77d4d037ef0f27b8efc9f9e9e2fcbab1963b8
Trọng Hiếu <hieu15904@gmail.com>
2026-09-04 13:33:27 +0700

    tích hợp trang tổng quan

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const accountRouters = require('./account.route');
+const dashboardRouters = require('./dashboard.route');
 
 router.use('/account', accountRouters);
+router.use('/dashboard', dashboardRouters);
 
 module.exports = router;
 
