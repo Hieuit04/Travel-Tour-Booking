55c729311ed4cceaeaa66da5390869f77116b575
Trọng Hiếu <hieu15904@gmail.com>
2026-09-04 14:15:40 +0700

    thêm order list và order edit"

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,11 +4,13 @@ const accountRouters = require('./account.route');
 const dashboardRouters = require('./dashboard.route');
 const categoryRouters = require('./category.route');
 const tourRouters = require('./tour.route');
+const orderRouters = require('./order.route');
 
 router.use('/account', accountRouters);
 router.use('/dashboard', dashboardRouters);
 router.use('/category', categoryRouters);
 router.use('/tour', tourRouters);
+router.use('/order', orderRouters);
 
 module.exports = router;
 
